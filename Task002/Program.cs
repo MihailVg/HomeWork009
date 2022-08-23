f31231cd5c012915380025e50ee0c8b1e3d0c6bb
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-08-23 13:11:02 +0300

    Task002 done

---
+++ Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = 1;
+int N = 15;
+int result = 0;
+
+void Sum(int x, int y, int z)
+{
+    if(x <= y) 
+    {
+        z += x;
+        Sum(x+1, y, z);
+    }
+    else
+    {
+    Console.WriteLine(z);
+    }
+}
+
+Sum(M, N, result);
